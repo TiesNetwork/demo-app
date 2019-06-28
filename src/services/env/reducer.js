50a42b8c84b5cb365fa4aa1562305944cb388e50
Ivan Vyatkin <vanya095@gmail.com>
2019-06-28 14:45:33 +0300

    Change titles.

--- src/services/env/reducer.js
+++ src/services/env/reducer.js
@@ -2,7 +2,7 @@
 import { SET_LOCALE } from './types';
 
 const initialState = {
-  locale: localStorage.getItem('locale') || 'en',
+  locale: localStorage.getItem('locale') || 'ru',
 };
 
 export default (state = initialState, action) => {
